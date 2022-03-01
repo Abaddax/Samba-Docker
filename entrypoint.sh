2bdf0f8d55df34c9a3771f415f1cc0bb3ad3cc61
max920117 <max920117@gmx.de>
2022-03-01 15:04:23 +0100

    Console output changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,15 @@ GREEN="\033[0;32m"
 NC="\033[0m"
 
 echo -e "$GREEN[ Running startup routine ]$NC"
-/etc/samba/custom/startup_routine $(id -u) $(id -g)
-echo -e "done\n"
+/startup_routine $(id -u) $(id -g)
+echo -e "\n"
 
 echo -e "$GREEN[ Starting samba ]$NC"
 echo "User:$(id -u)"
-smbd -DF --no-process-group --configfile /config/smb.conf -l /logs/ -d 3
-echo -e "done\n";
+echo "Running..."
+smbd -DF --no-process-group --configfile /config/smb.conf -l /logs/ -d 3 -p 4455
+echo -e "\n";
 
-exec "$@"
+#exec "$@"
+
+echo "Closing (if unexpected, read /logs/log.smbd for more information)"
